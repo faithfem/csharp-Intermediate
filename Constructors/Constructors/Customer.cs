9300f5ecce4eed27ba25ebf695cef93a3f4298f8
faithfem <fmugure@intracitygeeks.org>
2017-12-03 16:52:40 -0500

    Methods

---
+++ Constructors/Constructors/Customer.cs
@@ -0,0 +1,24 @@
+﻿namespace Constructors
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+
+        public Customer()
+        {
+
+        }
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class Point
+    {
+        public int X;
+        public int Y;
+
+        public Point (int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Mov(Point newLocation
+            {
+            this.X = newLocation.X;
+            this.Y = newLocation.Y;
+            }
+
+    }
+    
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = new Point(10, 20);
+            point.Move(new Point(40, 60));
+            Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+            point.Move(100, 200);
+            Console.WriteLine("Point is at {0}, {1})", point.X, point.Y);
+        }
+    }
+}
