87b3c5a00a2b3b96ba9f45af0de97cb6a6f305d8
faithfem <fmugure@intracitygeeks.org>
2017-12-03 19:38:48 -0500

    Sec2 Num13-Indexers

---
+++ Indexers/Indexers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    public class HttpCookie
+    {
+        private Dictionary<string, string> dictionary;
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+
+        public string this[string key]
+        {
+            get { return _dictionary[key]; }
+            set { _dictionary[key] = value; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
