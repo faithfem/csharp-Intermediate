87b7dd9b2a30137478f0397f9f527e122964bd25
faithfem <fmugure@intracitygeeks.org>
2017-12-04 20:37:32 -0500

    Section 4 Num 21-Access Modifiers Complete

---
+++ AccessModifiersii/AccessModifiersii/Program.cs
@@ -0,0 +1,28 @@
+﻿using Amazon;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiersii
+{
+    /*public class GoldCustomer : Customer
+    {
+
+        public void OfferVoucher()
+        {
+            this.
+        }
+    }*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(); //Need to move customer class to a separate assembly
+            Amazon.RateCalculator calculator = new RateCalculator();
+
+            System.Console.WriteLine();
+        }
+    }
+}
