586c27a5f3018693e66dea9047053eb85eb5f96d
faithfem <fmugure@intracitygeeks.org>
2017-12-08 21:53:23 -0500

    Section 5 Lecture 27-Method Overriding

---
+++ MethodOverriding/MethodOverriding/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MethodOverriding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ MethodOverriding/MethodOverriding/Shape.cs
@@ -0,0 +1,10 @@
+﻿namespace MethodOverriding
+{
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Heith { get; set; }
+        public int Position { get; set; }
+        public ShapeType Type { get; set; }
+    }
+}
