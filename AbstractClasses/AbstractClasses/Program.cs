7c94c3f0d5d5675983f0301c48d96480c17ddf61
faithfem <fmugure@intracitygeeks.org>
2017-12-08 22:43:24 -0500

    Section 5 Lecture 28-Abstract Classes & Members

---
+++ AbstractClasses/AbstractClasses/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace AbstractClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var shape = new Shape();
+
+            var circle = new Circle();
+            circle.Draw();
+
+            var rectangle = new Rectangle();
+            rectangle.Draw();
+                
+        }
+    }
+}
