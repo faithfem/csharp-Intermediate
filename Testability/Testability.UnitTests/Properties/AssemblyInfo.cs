bcd99268ed26aec1c6e19d63488b25ccd54e1089
faithfem <fmugure@intracitygeeks.org>
2017-12-08 23:47:44 -0500

    Section 5 Lecture 28

---
+++ Testability/Testability.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testability.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testability.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fe8c9d51-f3f8-42df-8450-a854aaef6ce6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Testability/Testability/ShippingCalculator.cs
@@ -0,0 +1,20 @@
+﻿namespace Testability
+{
+    public interface IShippingCalculator
+    {
+        float CalculateShipping(Order order);
+    }
+
+
+    public class ShippingCalculator: IShippingCalculator
+    {
+        public float CalculateShipping (Order order)
+        {
+            if (order.TotalPrice < 30f)
+                return order.TotalPrice * 0.1f;
+
+            return 0; //IF ORDER TOTAL IS > $30, THEN FREE SHIPPING
+        }
+
+    }
+}
